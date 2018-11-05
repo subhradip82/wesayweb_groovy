ba6996162398558d079750547269b69265b6ae64
subhradip.bose@saama.com <subhradip.bose@gmailc.om>
2018-11-05 18:13:07 +0530

    added dependencies

---
+++ src/main/groovy/com/wesayweb/application/WesayWebApplication.groovy
@@ -0,0 +1,14 @@
+package com.wesayweb.application
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import com.wesayweb.application.WesayWebApplication
+
+@SpringBootApplication
+class WesayWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run WesayWebApplication, args
+	}
+}
---
+++ src/test/groovy/com/wesayweb/WesayWeb/WesayWebApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.wesayweb.WesayWeb
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class WesayWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
